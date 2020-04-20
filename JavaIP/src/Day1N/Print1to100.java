044de28a8b92402e3fc44007fccfe819f6e11672
devnandeeshcl <devnandeeshclakhi@gmail.com>
2020-04-20 12:38:28 +0530

    Day 1 program added

---
+++ JavaIP/src/Day1N/Print1to100.java
@@ -0,0 +1,9 @@
+package Day1N;
+
+public class Print1to100 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hai");
+	}
+
+}
---
+++ JavaIP/src/day1A/Print1to100.java
@@ -0,0 +1,5 @@
+package day1A;
+
+public class Print1to100 {
+
+}
