c37993b2df5ff3f7afbda94f994deb903a60d402
devnandeeshcl <devnandeeshclakhi@gmail.com>
2020-04-20 12:55:45 +0530

    print1to100 program added

--- JavaIP/src/Day1N/Print1to100.java
+++ JavaIP/src/Day1N/Print1to100.java
@@ -1,9 +1,24 @@
 package Day1N;
 
+class Print {
+
+   
+	static void p(int n) {
+		
+		if (n <= 100) {
+			System.out.print(n + " "); // 1 2 3 .... 100
+			n = n + 1; // 1=1+1=2 3 -----100 101
+			p(n);
+		}
+	}
+}
+
 public class Print1to100 {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hai");
+
+		// Print a = new Print();
+		Print.p(1);
 	}
 
 }
